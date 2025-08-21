f7cd64213417cd6e1eeaf8f168608f00ad215319
Argeoff <214428272+Argeoff@users.noreply.github.com>
2025-08-21 14:48:56 +0800

    created a 16x16 grid, kinda silly way though

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#con");
+
+for (let i = 1; i < 257; i++){
+    const newDiv = document.createElement("div");
+    newDiv.id = "div" + i;
+    newDiv.classList.add("div" + i);
+    container.appendChild(newDiv);
+};
