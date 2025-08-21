2d17f4000304d2266576674137155a028b434116
Argeoff <214428272+Argeoff@users.noreply.github.com>
2025-08-21 15:22:00 +0800

    added a clear button to clear drawing

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const container = document.querySelector("#con");
+const gridButton= document.querySelector(".grid");
+const clearButton= document.querySelector(".clear");
+
+gridButton.addEventListener("click", () => {
+    const answer = prompt("How many rows/columns do you want?")
+});
+
+clearButton.addEventListener("click", () => {
+    const allDivs = document.querySelectorAll("div");
+
+    allDivs.forEach(div => {
+    div.style.backgroundColor = "white";
+    })
+});
 
 for (let i = 1; i < 257; i++){
     const newDiv = document.createElement("div");
-    newDiv.id = "div" + i;
     newDiv.classList.add("div" + i);
     container.appendChild(newDiv);
 
